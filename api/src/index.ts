60b5b09ee542985e67e5228eb1235af53f147306
Thomas Charier <thomascharier@hotmail.fr>
2025-12-03 14:20:27 +0100

    package

--- api/src/index.ts
+++ api/src/index.ts
@@ -15,7 +15,7 @@ import userRoutes from "../routers/userRoutes";
 const app = express();
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
